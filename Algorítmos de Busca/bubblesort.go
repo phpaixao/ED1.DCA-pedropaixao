6d27bef34b47b3587d6e1928f762ca6ac149f179
Pedro Henrique <pedrinho.paixaosilva@gmail.com>
2025-09-24 22:12:33 -0300

    Refactor BubbleSort function to improve efficiency by using a flag to track swaps

--- Algorítmos de Busca/bubblesort.go
+++ Algorítmos de Busca/bubblesort.go
@@ -6,16 +6,16 @@ import(
 
 func BubbleSort(v []int) {
 	for i := 0; i<len(v)-1; i++{
-		cont := 0
+		flag := false
 		for j := 0; j<(len(v)-i-1); j++{
 			if v[j] > v[j+1]{
 				v[j], v[j+1] = v[j+1], v[j]
-				cont++
+				flag = true
 			}
 		}
 		// Teste pra saber quantas varreduras foram feitas
 		fmt.Println("Executado!") 
-		if cont == 0 {return}
+		if !flag {return}
 	}
 }
 
